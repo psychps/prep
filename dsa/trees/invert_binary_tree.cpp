b8a3fc8c9a4119070e86ff47186dcdbe8734e092
psychps <ps.sahni111@gmail.com>
2026-02-25 01:35:38 +0530

    invert tree solve

---
+++ dsa/trees/invert_binary_tree.cpp
@@ -0,0 +1,14 @@
+// Time: O(n)
+// Space: O(n)
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return nullptr;
+        
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        return root;
+    }
+};
